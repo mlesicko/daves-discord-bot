fd6fe6668b7d4feb668e28d6f40ee2a1b76d675f
Matt <loggerisms@gmail.com>
2019-08-08 19:33:56 -0500

    Actual fix for 69 exploding head issue.

--- actions/sixtyNine.js
+++ actions/sixtyNine.js
@@ -2,7 +2,7 @@ const run = ({messageText, actions}) => {
 	const {sendMessage} = actions;
 	// Fix for issue displaying response to invisible 69s.
 	const cleanText = messageText.replace(/<[^>]*>/g, '');
-	if (messageText.includes('69')) {
+	if (cleanText.includes('69')) {
 		sendMessage('Nice!');
 		return true;
 	} else {
