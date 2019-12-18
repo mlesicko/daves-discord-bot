270b011a19b4f4fc8523b619cf38c1f806d31073
Matt <loggerisms@gmail.com>
2019-12-18 14:09:42 -0600

    Fixed bug where bot couldn't parse itself being tagged correctly.

--- commands/patchNotes.js
+++ commands/patchNotes.js
@@ -11,22 +11,7 @@ const run = ({messageText, sendMessage}) => {
 }
 
 const patchNotes = 
-	'8/28/19:\n' +
-	'• Shaming is channel-specific.\n' +
-	'• Bot no longer talks to itself.\n' +
-	'8/18/19:\n' +
-	'• Shame command added.\n' +
-	'8/08/19:\n' +
-	'• Improved database logic.\n' +
-	'• Fix for exploding head 69 bug.\n' +
-	'8/03/19:\n' +
-	'• Added names command.\n' +
-	'• Added report bug command.\n' +
-	'8/02/19:\n' +
-	'• Added mute/unmute commands.\n' +
-	'• Added kill command.\n' +
-	'• Added patch notes command.\n' +
-	'• Added help command.\n' +
-	'• Added advice command.\n';
+	'2019/12/18\n' +
+	'• Fixed bug where bot got confused by its own name.\n';
 
 module.exports=run;
